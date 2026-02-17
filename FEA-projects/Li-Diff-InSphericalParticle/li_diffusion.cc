145770136c84b0c71a0e06d710ad1599b6c32e84
DigoShane <47403292+DigoShane@users.noreply.github.com>
2026-02-16 23:27:51 -0600

    added new code.

---
+++ FEA-projects/Li-Diff-InSphericalParticle/li_diffusion.cc
@@ -0,0 +1,146 @@
+#include <deal.II/base/function.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/timer.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/numerics/data_out.h>
+#include <fstream>
+
+using namespace dealii;
+
+class LithiumDiffusion
+{
+public:
+  LithiumDiffusion();
+  void run();
+
+private:
+  void setup_system();
+  void assemble_system();
+  void solve_time_step();
+  void output_results(const unsigned int time_step);
+
+  Triangulation<2> triangulation;
+  FE_Q<2>          fe;
+  DoFHandler<2>    dof_handler;
+
+  SparsityPattern      sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+
+  Vector<double> solution;
+  Vector<double> old_solution;
+  Vector<double> system_rhs;
+
+  double time;
+  double time_step;
+  const double D = 1e-14;  // diffusivity
+};
+
+LithiumDiffusion::LithiumDiffusion()
+  : fe(1), dof_handler(triangulation)
+{
+  time = 0;
+  time_step = 1.0;
+}
+
+void LithiumDiffusion::setup_system()
+{
+  dof_handler.distribute_dofs(fe);
+
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dsp);
+  sparsity_pattern.copy_from(dsp);
+
+  system_matrix.reinit(sparsity_pattern);
+  solution.reinit(dof_handler.n_dofs());
+  old_solution.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+}
+
+void LithiumDiffusion::assemble_system()
+{
+  QGauss<2> quadrature_formula(2);
+  FEValues<2> fe_values(fe, quadrature_formula,
+                        update_values | update_gradients |
+                        update_JxW_values);
+
+  const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+  const unsigned int n_q_points    = quadrature_formula.size();
+
+  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+  Vector<double>     cell_rhs(dofs_per_cell);
+
+  for (const auto &cell : dof_handler.active_cell_iterators())
+  {
+    fe_values.reinit(cell);
+    cell_matrix = 0;
+    cell_rhs = 0;
+
+    for (unsigned int q=0; q<n_q_points; ++q)
+      for (unsigned int i=0; i<dofs_per_cell; ++i)
+      {
+        for (unsigned int j=0; j<dofs_per_cell; ++j)
+        {
+          cell_matrix(i,j) +=
+            (fe_values.shape_value(i,q) *
+             fe_values.shape_value(j,q)
+             +
+             time_step * D *
+             fe_values.shape_grad(i,q) *
+             fe_values.shape_grad(j,q))
+            * fe_values.JxW(q);
+        }
+
+        cell_rhs(i) +=
+          fe_values.shape_value(i,q) *
+          old_solution(cell->vertex_dof_index(i,0)) *
+          fe_values.JxW(q);
+      }
+
+    cell->get_dof_indices(local_dof_indices);
+    for (unsigned int i=0; i<dofs_per_cell; ++i)
+      for (unsigned int j=0; j<dofs_per_cell; ++j)
+        system_matrix.add(local_dof_indices[i],
+                          local_dof_indices[j],
+                          cell_matrix(i,j));
+  }
+}
+
+void LithiumDiffusion::solve_time_step()
+{
+  SolverControl solver_control(1000, 1e-12);
+  SolverCG<> solver(solver_control);
+  solver.solve(system_matrix, solution, system_rhs,
+               PreconditionIdentity());
+}
+
+void LithiumDiffusion::run()
+{
+  GridGenerator::hyper_ball(triangulation);
+  triangulation.refine_global(4);
+
+  setup_system();
+
+  for (unsigned int t=0; t<100; ++t)
+  {
+    assemble_system();
+    solve_time_step();
+    old_solution = solution;
+    output_results(t);
+  }
+}
+
+int main()
+{
+  LithiumDiffusion problem;
+  problem.run();
+}
+
